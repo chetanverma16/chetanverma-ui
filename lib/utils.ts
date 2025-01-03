5e6e9cdf48efc5c3a45e815ad2a1fd94e15cb98d
Chetan Verma <chetan61verma@gmail.com>
2025-01-03 15:19:15 +0530

    updates

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,44 +6,19 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function helperSearch(
-  query: string,
-  node: EachRoute,
-  prefix: string,
-  currenLevel: number,
-  maxLevel?: number
-) {
+export function helperSearch(query: string, node: EachRoute, prefix: string) {
   const res: EachRoute[] = [];
-  let parentHas = false;
-
   const nextLink = `${prefix}${node.href}`;
+
   if (!node.noLink && node.title.toLowerCase().includes(query.toLowerCase())) {
-    res.push({ ...node, items: undefined, href: nextLink });
-    parentHas = true;
+    res.push({ ...node, href: nextLink });
   }
-  const goNext = maxLevel ? currenLevel < maxLevel : true;
-  if (goNext)
-    node.items?.forEach((item) => {
-      const innerRes = helperSearch(
-        query,
-        item,
-        nextLink,
-        currenLevel + 1,
-        maxLevel
-      );
-      if (!!innerRes.length && !parentHas && !node.noLink) {
-        res.push({ ...node, items: undefined, href: nextLink });
-        parentHas = true;
-      }
-      res.push(...innerRes);
-    });
+
   return res;
 }
 
 export function advanceSearch(query: string) {
-  return ROUTES.map((node) =>
-    helperSearch(query, node, "", 1, query.length == 0 ? 2 : undefined)
-  ).flat();
+  return ROUTES.map((node) => helperSearch(query, node, "")).flat();
 }
 
 // Thursday, May 23, 2024
